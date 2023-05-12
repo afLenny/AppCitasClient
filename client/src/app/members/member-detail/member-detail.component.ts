b49178d4bd6bbe006ee11270d3e51ec8976ae993
Lenny <bsegundopm@gmail.com>
2023-05-12 14:55:12 -0600

    Project to github and selecttab Messages feature

---
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -0,0 +1,87 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery';
+import { TabDirective, TabsetComponent } from 'ngx-bootstrap/tabs';
+import { Member } from 'src/app/_models/member';
+import { Message } from 'src/app/_models/message';
+import { MembersService } from 'src/app/_services/members.service';
+import { MessageService } from 'src/app/_services/message.service';
+
+@Component({
+  selector: 'app-member-detail',
+  templateUrl: './member-detail.component.html',
+  styleUrls: ['./member-detail.component.css']
+})
+export class MemberDetailComponent implements OnInit {
+  @ViewChild('memberTabs') memberTabs?: TabsetComponent;
+  member: Member | undefined;
+  galleryOptions: NgxGalleryOptions[] = [];
+  galleryImages: NgxGalleryImage[] = [];
+  activateTab?: TabDirective;
+  messages: Message[] = [];
+
+  constructor(private membersService: MembersService, private route: ActivatedRoute, private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.loadMember();
+
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false
+      }
+    ];
+  }
+
+
+  getImages(): { small: string; medium: string; big: string; }[] {
+    if (!this.member) return [];
+    const imageUrls = [];
+    for (const photo of this.member.photos) {
+      imageUrls.push({
+        small: photo.url,
+        medium: photo.url,
+        big: photo.url
+      });
+    }
+    return imageUrls;
+  }
+
+  loadMember() {
+    const username = this.route.snapshot.paramMap.get('username');
+    if (!username) return;
+    this.membersService.getMember(username).subscribe({
+      next: member => {
+        this.member = member;
+        this.galleryImages = this.getImages();
+      }
+    });
+  }
+
+  selectTab(heading: string){
+    if(this.memberTabs){
+      this.memberTabs.tabs.find(x => x.heading === heading)!.active = true;
+    }
+  }
+
+  loadMessages(){
+    if(this.member)
+    {
+      this.messageService.getMessagesThread(this.member.username).subscribe({
+        next: messages => this.messages = messages
+      });
+    }
+  }
+
+  onTabActivated(data: TabDirective){
+    this.activateTab = data;
+    if(this.activateTab.heading == 'Messages'){
+      this.loadMessages();
+    }
+  }
+
+}
